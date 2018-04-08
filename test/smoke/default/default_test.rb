149541a7c9c7cc23a7dde6db828fda84c3eb91e2
Jon Middleton <jon.j.middleton@gmail.com>
2018-04-08 14:42:27 +0100

    Add inspec intergration tests

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,18 +1,60 @@
-# # encoding: utf-8
+# encoding: utf-8
+# author: Jon Middleton
 
-# Inspec test for recipe desalination::default
+control '01' do
+  impact 1.0
+  title 'Verify salt-minion service'
+  desc 'Ensures salt-minion service is not running'
 
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
-
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+  describe service('salt-minion') do
+    it { should_not be_enabled }
+    it { should_not be_installed }
+    it { should_not be_running }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+control '02' do
+  impact 1.0
+  title 'Verify salt-minion Package'
+  desc 'Ensures salt-minion package is not installed'
+
+  describe package('salt-minion') do
+    it { should_not be_installed }
+  end
+
+  if os.family == 'redhat'
+    describe package('salt') do
+      it { should_not be_installed }
+    end
+  elsif os.family == 'debian'
+    describe package('salt-common') do
+      it { should_not be_installed }
+    end
+  end
+end
+
+control '03' do
+  impact 0.5
+  title 'Verify salt config'
+  desc 'Ensures salt is not configured'
+
+  describe directory('/etc/salt/') do
+    it { should_not exist }
+  end
+end
+
+control '04' do
+  impact 0.5
+  title 'Verify salt repos are missing'
+  desc 'Ensures Salt cannot be installed via package tools'
+
+  if os.family == 'redhat'
+    describe package('salt-repo') do
+      it { should_not be_installed }
+    end
+  elsif os.family == 'debian'
+    describe file('/etc/apt/sources.list.d/saltstack.list') do
+      it { should_not exist }
+    end
+  end
 end
